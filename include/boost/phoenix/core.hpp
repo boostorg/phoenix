c9c1005dcf686291f48edec7245cad117900b396
Eric Niebler <eric@boostpro.com>
2008-06-10 18:05:00 +0000

    include as_actor
    [SVN r46302]

--- include/boost/phoenix/core.hpp
+++ include/boost/phoenix/core.hpp
@@ -11,7 +11,7 @@
 #include <boost/phoenix/core/limits.hpp>
 //#include <boost/phoenix/core/basic_environment.hpp>
 #include <boost/phoenix/core/actor.hpp>
-//#include <boost/phoenix/core/as_actor.hpp>
+#include <boost/phoenix/core/as_actor.hpp>
 //#include <boost/phoenix/core/is_actor.hpp>
 #include <boost/phoenix/core/argument.hpp>
 #include <boost/phoenix/core/value.hpp>
