1b02224e751b8cff58e9beff5b5e6fba25cd2788
Thomas Heller <thom.heller@googlemail.com>
2010-07-09 00:10:00 +0000

    adapted header
    [SVN r63773]

--- include/boost/phoenix/statement/switch.hpp
+++ include/boost/phoenix/statement/switch.hpp
@@ -1,5 +1,5 @@
 /*==============================================================================
-    Copyright (c) 2005-2010 Joel de Guzman
+    Copyright (c) 2001-2010 Joel de Guzman
     Copyright (c) 2010 Thomas Heller
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
