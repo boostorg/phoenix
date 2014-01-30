412a1d6292ad7488aead8efbef1fc8462c64aee1
John Fletcher <J.P.Fletcher@aston.ac.uk>
2014-01-30 22:57:09 +0000

    test/statement/bug5715.cpp Added copyright and licence.

--- test/statement/bug5715.cpp
+++ test/statement/bug5715.cpp
@@ -1,3 +1,12 @@
+/*=============================================================================
+    Copyright (c) 2005-2007 Dan Marsden
+    Copyright (c) 2005-2007 Joel de Guzman
+    Copyright (c) 2014      John Fletcher
+    
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
 // Check for Bug5715
 
 #include <boost/phoenix/statement/sequence.hpp>
