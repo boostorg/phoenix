6afbadabd783d6671eed0452c67d35f2fba48b4b
John Fletcher <J.P.Fletcher@aston.ac.uk>
2014-01-31 19:47:44 +0000

    include/boost/phoenix/version.hpp Change version to 3.0.2

--- include/boost/phoenix/version.hpp
+++ include/boost/phoenix/version.hpp
@@ -13,6 +13,6 @@
 //  This is the version of the library
 //
 ///////////////////////////////////////////////////////////////////////////////
-#define BOOST_PHOENIX_VERSION   0x3001    // 3.0.1
+#define BOOST_PHOENIX_VERSION   0x3002    // 3.0.2
 
 #endif
