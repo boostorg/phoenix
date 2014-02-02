9d224c7330b44431cbac620e1d89817a4a3b17b2
John Fletcher <J.P.Fletcher@aston.ac.uk>
2014-02-02 11:21:15 +0000

    test/regression/bug7624.cpp  C++11 conditional solution

--- test/regression/bug7624.cpp
+++ test/regression/bug7624.cpp
@@ -1,12 +1,18 @@
 #include <boost/phoenix.hpp>
 #include <boost/range/as_literal.hpp>
+#include <boost/detail/lightweight_test.hpp>
 
 using namespace boost::phoenix::placeholders;
 using namespace boost::phoenix;
     
 int main()
 {
-    find(boost::as_literal("fox"), 'x')();                      // works
-    find(boost::as_literal("fox"), construct<char>(arg1))('x'); // works
-    find(boost::as_literal("fox"), arg1)('x');                  // compilation error
+    char X('x'), Y ;
+    find(boost::as_literal("fox"), 'x')();     // works
+#ifndef BOOST_NO_CXX11_DECLTYPE
+    Y = find(boost::as_literal("fox"), arg1)('x'); // works for C++11
+#else
+    Y = find(boost::as_literal("fox"), construct<char>(arg1))('x'); // works
+#endif
+    BOOST_TEST(X==Y);
 }
