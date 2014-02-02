c9604ef268682f752afa0f140fee61a622e9263f
John Fletcher <J.P.Fletcher@aston.ac.uk>
2014-02-02 11:35:18 +0000

    test/Jamfile resolve changes

---
+++ test/regression/bug7624.cpp
@@ -0,0 +1,19 @@
+#include <boost/phoenix.hpp>
+#include <boost/range/as_literal.hpp>
+#include <boost/detail/lightweight_test.hpp>
+
+using namespace boost::phoenix::placeholders;
+using namespace boost::phoenix;
+    
+int main()
+{
+  char X('x');
+    find(boost::as_literal("fox"), 'x')();     // works
+#ifndef BOOST_NO_CXX11_DECLTYPE
+    const char *Y = find(boost::as_literal("fox"), arg1)('x'); // works for C++11
+#else
+    const char *Y = find(boost::as_literal("fox"), construct<char>(arg1))('x'); // works
+#endif
+    BOOST_TEST(X == *Y);
+
+}
