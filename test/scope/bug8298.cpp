45cca5583390bc934153fcfb9a7137dc5f8a9973
John Fletcher <J.P.Fletcher@aston.ac.uk>
2014-02-07 23:00:38 +0000

    test/scope/8298.cpp remove header

--- test/scope/bug8298.cpp
+++ test/scope/bug8298.cpp
@@ -4,7 +4,6 @@
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 
-#include <iostream>
 #include <boost/phoenix.hpp>
 
 #include <boost/detail/lightweight_test.hpp>
