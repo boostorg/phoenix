eda8661ad31e688b8c1a5304599dff4703e4e429
John Fletcher <J.P.Fletcher@aston.ac.uk>
2015-01-23 12:12:17 +0000

    bind_void_dm_test.cpp New test not yet working.

--- test/boost_bind_compatibility/bind_void_dm_test.cpp
+++ test/boost_bind_compatibility/bind_void_dm_test.cpp
@@ -1,3 +1,12 @@
+/*==============================================================================
+    Copyright (c) 2006 Peter Dimov
+    Copyright (c) 2014 Agustin Berge
+    Copyright (c) 2015 John Fletcher
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
 #include <boost/config.hpp>
 
 #if defined(BOOST_MSVC)
@@ -7,18 +16,8 @@
 #pragma warning(disable: 4514)  // unreferenced inline removed
 #endif
 
-//
-//  bind_void_mf_test.cpp - test for bind<void> with member functions
-//
-//  Copyright (c) 2008 Peter Dimov
-//  Copyright (c) 2014 Agustin Berge
-//
-// Distributed under the Boost Software License, Version 1.0. (See
-// accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-//
-
-#include <boost/bind.hpp>
+#include <boost/phoenix/core.hpp>
+#include <boost/phoenix/bind.hpp>
 #include <boost/ref.hpp>
 
 #if defined(BOOST_MSVC) && (BOOST_MSVC < 1300)
@@ -42,26 +41,28 @@ struct Z
 
 void member_data_test()
 {
+    using boost::phoenix::bind;
+
     Z z = { 17041 };
     Z * pz = &z;
 
-    boost::bind<void>( &Z::m, _1 )( z );
-    boost::bind<void>( &Z::m, _1 )( pz );
+    bind<void>( &Z::m, _1 )( z );
+    bind<void>( &Z::m, _1 )( pz );
 
-    boost::bind<void>( &Z::m, z )();
-    boost::bind<void>( &Z::m, pz )();
-    boost::bind<void>( &Z::m, boost::ref(z) )();
+    bind<void>( &Z::m, z )();
+    bind<void>( &Z::m, pz )();
+    bind<void>( &Z::m, boost::ref(z) )();
 
 
     Z const cz = z;
     Z const * pcz = &cz;
 
-    boost::bind<void>( &Z::m, _1 )( cz );
-    boost::bind<void>( &Z::m, _1 )( pcz );
+    bind<void>( &Z::m, _1 )( cz );
+    bind<void>( &Z::m, _1 )( pcz );
 
-    boost::bind<void>( &Z::m, cz )();
-    boost::bind<void>( &Z::m, pcz )();
-    boost::bind<void>( &Z::m, boost::ref(cz) )();
+    bind<void>( &Z::m, cz )();
+    bind<void>( &Z::m, pcz )();
+    bind<void>( &Z::m, boost::ref(cz) )();
 }
 
 int main()
