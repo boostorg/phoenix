9b40b88fa192e00266358a5c8d42b6b6accf45a2
John Fletcher <J.P.Fletcher@aston.ac.uk>
2015-02-08 17:18:30 +0000

    bind_interoperation_test extended to more cases.

--- test/boost_bind_compatibility/bind_interoperation_test.cpp
+++ test/boost_bind_compatibility/bind_interoperation_test.cpp
@@ -44,9 +44,78 @@ int f2( int x, int y )
     return x + y;
 }
 
+struct X
+{
+  mutable int n;
+
+  X() : n(0) {}
+
+    int f0() { n += f1(17); return n; }
+    int g0() const { n += g1(17); return n; }
+
+
+    int f1(int a1) {  return a1; }
+    int g1(int a1) const { return a1; }
+};
+
+struct Y
+{
+    int m;
+};
+
 namespace phx = boost::phoenix;
 using phx::placeholders::arg1;
 using phx::placeholders::arg2;
+using boost::phoenix::ref;
+
+void member_test()
+{
+    Y y = { 17041 };
+    Y * py = &y;
+
+    BOOST_TEST( boost::bind( &Y::m, _1 )( y ) == 17041 );
+    BOOST_TEST( boost::bind( &Y::m, _1 )( py ) == 17041 );
+
+    BOOST_TEST( phx::bind( &Y::m, _1 )( y ) == 17041 );
+    BOOST_TEST( phx::bind( &Y::m, _1 )( py ) == 17041 );
+
+    BOOST_TEST( phx::bind( &Y::m, arg1 )( y ) == 17041 );
+    BOOST_TEST( phx::bind( &Y::m, arg1 )( py ) == 17041 );
+
+    BOOST_TEST( boost::bind( &Y::m, y )() == 17041 );
+    BOOST_TEST( boost::bind( &Y::m, py )() == 17041 );
+    //BOOST_TEST( boost::bind( &Y::m, ref(y) )() == 17041 );
+
+    BOOST_TEST( phx::bind( &Y::m, y )() == 17041 );
+    BOOST_TEST( phx::bind( &Y::m, py )() == 17041 );
+    BOOST_TEST( phx::bind( &Y::m, ref(y) )() == 17041 );
+
+    return;
+}
+
+void member_function_test()
+{
+
+    X x;
+
+    // 0
+
+    BOOST_TEST(boost::bind(&X::f0, &x )() == 17);
+    //boost::bind(&X::f0, ref(x) )(); boost::bind does not work with phx::ref.
+
+    BOOST_TEST(boost::bind(&X::g0, &x )() == 34);
+    BOOST_TEST(boost::bind(&X::g0, x )()  == 51);
+    //boost::bind(&X::g0, ref(x) )();
+
+    BOOST_TEST(phx::bind(&X::f0, &x )()   == 51);
+    BOOST_TEST(phx::bind(&X::f0, ref(x) )() == 68);
+
+    BOOST_TEST(phx::bind(&X::g0, &x )()   == 85);
+    BOOST_TEST(phx::bind(&X::g0, x )()    == 102);
+    BOOST_TEST(phx::bind(&X::g0, ref(x) )() == 102);
+
+    return;
+}
 
 int main()
 {
@@ -67,5 +136,7 @@ int main()
   BOOST_TEST( fun2_f2(2,3) == 5 );
   BOOST_TEST( fun3_f2(3,4) == 7 );
  
+  member_function_test();
+  member_test();
   return boost::report_errors();
 }
