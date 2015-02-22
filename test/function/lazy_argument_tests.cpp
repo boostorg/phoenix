0f215c97be5797ef76a4774d0c58cedf0d1ff9e9
John Fletcher <J.P.Fletcher@aston.ac.uk>
2015-02-22 18:29:44 +0000

    function/lazy_argument_tests.cpp Build tests.

--- test/function/lazy_argument_tests.cpp
+++ test/function/lazy_argument_tests.cpp
@@ -17,3 +17,37 @@
 #include <boost/detail/lightweight_test.hpp>
 #include <boost/phoenix/core.hpp>
 #include <boost/phoenix/function.hpp>
+
+namespace example {
+      struct G {
+
+            template <typename Sig>
+            struct result;
+
+            template <typename This, typename A0>
+            struct result<This(A0)>
+               : boost::remove_reference<A0>
+            {};
+
+            template <typename T>
+            T operator()(T t) const { return ++t; }
+
+      };
+}
+
+typedef boost::phoenix::function<example::G> GG;
+boost::phoenix::function<example::G> gg;
+
+template <typename F,typename T>
+T h(F f, T const& t)
+{
+  return f(t)();
+}
+
+int main()
+{
+  BOOST_TEST(h(gg,1) ==2);
+  BOOST_TEST(h<GG,int>(gg,1) ==2);
+
+  return boost::report_errors();
+}
