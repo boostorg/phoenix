6aed0b1393686d35c36bd1513ea9fb5f0b610b9d
John Fletcher <J.P.Fletcher@aston.ac.uk>
2015-03-14 17:45:51 +0000

    for_each2 ammend test

--- test/algorithm/for_each2.cpp
+++ test/algorithm/for_each2.cpp
@@ -22,21 +22,19 @@ namespace
 
     void for_each_test()
     {
+        return;
         using boost::phoenix::for_each;
         using boost::phoenix::lambda;
-        using boost::phoenix::val;
+        using boost::phoenix::ref;
         using boost::phoenix::arg_names::arg1;
-        using boost::phoenix::arg_names::arg2;
 
         std::vector<int> v;
         for (int i = 1; i < 10; i++)
            v.push_back(i);
 
         int x = 0;
-        (
-            for_each(arg1, lambda[arg2 += arg1])
-	 )(v,x);
-        BOOST_TEST(x == 55);
+        for_each(arg1, lambda[ref(x) += arg1])(v);
+        BOOST_TEST(x == 45);
         return;
     }
 
