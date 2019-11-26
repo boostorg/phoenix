8b4cb05a4da41a819c8709ffef3846b7f4db0f2a
Nikita Kniazev <nok.raven@gmail.com>
2019-11-27 00:44:33 +0300

    Move debug.hpp out of core

--- include/boost/phoenix/debug.hpp
+++ include/boost/phoenix/debug.hpp
@@ -6,8 +6,8 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
-#ifndef BOOST_PHOENIX_CORE_DEBUG_HPP
-#define BOOST_PHOENIX_CORE_DEBUG_HPP
+#ifndef BOOST_PHOENIX_DEBUG_HPP
+#define BOOST_PHOENIX_DEBUG_HPP
 
 #include <iostream>
 #include <boost/phoenix/version.hpp>
