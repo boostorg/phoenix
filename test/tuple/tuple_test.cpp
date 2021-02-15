79cd20118b7856bb782dba8abb50b7e6d207c25e
Beojan Stanislaus <beojan.stanislaus@physics.ox.ac.uk>
2021-02-15 10:12:36 +0000

    Use direct initialization in tuple_test.cpp

--- test/tuple/tuple_test.cpp
+++ test/tuple/tuple_test.cpp
@@ -6,28 +6,28 @@
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 #include <iostream>
-#if __cplusplus >= 201402L  || (defined(_MSC_FULL_VER) && _MSC_FULL_VER >= 190024210)
+#if __cplusplus >= 201402L || (defined(_MSC_FULL_VER) && _MSC_FULL_VER >= 190024210)
 
+#include <boost/detail/lightweight_test.hpp>
 #include <boost/phoenix/core.hpp>
 #include <boost/phoenix/stl/tuple.hpp>
-#include <boost/detail/lightweight_test.hpp>
 
 #include <tuple>
 
 namespace {
-    void tuple_get_test() {
-        using boost::phoenix::get_;
-        using boost::phoenix::arg_names::arg1;
-        std::tuple<int, double, char> tup = {1, 1.6, 'b'};
-        BOOST_TEST(get_<0>(arg1)(tup) == std::get<0>(tup));
-        BOOST_TEST(get_<1>(arg1)(tup) == std::get<1>(tup));
-        BOOST_TEST(get_<2>(arg1)(tup) == std::get<2>(tup));
-        BOOST_TEST(get_<int>(arg1)(tup) == std::get<int>(tup));
-        BOOST_TEST(get_<double>(arg1)(tup) == std::get<double>(tup));
-        BOOST_TEST(get_<char>(arg1)(tup) == std::get<char>(tup));
-        return;
-    }
+void tuple_get_test() {
+    using boost::phoenix::get_;
+    using boost::phoenix::arg_names::arg1;
+    std::tuple<int, double, char> tup{1, 1.6, 'b'};
+    BOOST_TEST(get_<0>(arg1)(tup) == std::get<0>(tup));
+    BOOST_TEST(get_<1>(arg1)(tup) == std::get<1>(tup));
+    BOOST_TEST(get_<2>(arg1)(tup) == std::get<2>(tup));
+    BOOST_TEST(get_<int>(arg1)(tup) == std::get<int>(tup));
+    BOOST_TEST(get_<double>(arg1)(tup) == std::get<double>(tup));
+    BOOST_TEST(get_<char>(arg1)(tup) == std::get<char>(tup));
+    return;
 }
+} // namespace
 
 int main() {
     tuple_get_test();
