50973dc10ea16931245ea61a00b2ce9041acc5ba
Stephan Bergmann <sbergman@redhat.com>
2023-01-04 17:33:31 +0100

    Avoid boost::phoenix::placeholders::uarg1..10 ODR violations
    Those variables, defined in an include file, had external linkage, causing ODR
    violations.  Make them const to implicitly give them internal linkage.

--- include/boost/phoenix/stl/tuple.hpp
+++ include/boost/phoenix/stl/tuple.hpp
@@ -110,7 +110,7 @@ namespace boost { namespace phoenix {
     namespace placeholders {
         #define BOOST_PP_LOCAL_LIMITS (1, BOOST_PHOENIX_ARG_LIMIT)
         #define BOOST_PP_LOCAL_MACRO(N)                                                \
-            auto uarg##N =                                                             \
+            auto const uarg##N =                                                       \
             boost::phoenix::get_<(N)-1>(boost::phoenix::placeholders::arg1);
         #include BOOST_PP_LOCAL_ITERATE()
     }
